19252ee54563190b206ac513445f5fa604a7e2c9
oresh247 <oresh247@gmail.com>
2022-12-28 17:28:53 +0300

    new version

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+# coding:utf-8
+import json
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# глобальные переменные
+file_name_edto = 'edto.json'  # Файл eDTO, из которого переносим описание полей
+file_name_swagger = 'swagger.json'  # Файл swagger, в который добавляем описание
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Заполняем словарь с описание полей
+def change_def(d, targetkey, parenttargetkey, blokName):
+    if not isinstance(d, (dict, list)):
+        if targetkey == 'description':
+            return d
+        return d
+    elif isinstance(d, list):
+        return [v for v in (change_def(v, targetkey, parenttargetkey, blokName) for v in d)]
+    else:
+        return {k: v for k, v in ((k, change_def(v, k, targetkey,  blokName)) for k, v in d.items())}
 
+
+# Загружаем файл eDTO
+with open(file_name_edto, encoding='utf-8') as fh:
+    modelDefinitions = json.load(fh)['definitions']
+
+# Перебираем все eDTO и считываем описание для полей с привязкой к ДТО
+for key in modelDefinitions:
+    change_def(modelDefinitions[key], key, key, key)
 
